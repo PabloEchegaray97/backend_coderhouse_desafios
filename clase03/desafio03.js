a7e554a77ff51c13f1c1ef100a7f66a636c37b0c
pae <pablo.echegaray97@gmail.com>
2023-06-07 07:07:09 -0300

    desafio03(tests-deleteProduct())

--- clase03/desafio03.js
+++ clase03/desafio03.js
@@ -8,53 +8,93 @@
 //  - stock (número de piezas disponibles)
 //✓Debe tener un método addProduct el cual debe recibir un objeto con el formato previamente especificado, asignarle un id autoincrementable y guardarlo en el arreglo (recuerda siempre guardarlo como un array en el archivo).
 //✓ Debe tener un método getProducts, el cual debe leer el archivo de productos y devolver todos los productos en formato de arreglo. 
-//*X* Debe tener un método getProductById, el cual debe recibir un id, y tras leer el archivo, debe buscar el producto con el id especificado y devolverlo en formato objeto
-//*X*Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID 
-//*X* Debe tener un método deleteProduct, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo.
-const fs = require('fs');
+//✓ Debe tener un método getProductById, el cual debe recibir un id, y tras leer el archivo, debe buscar el producto con el id especificado y devolverlo en formato objeto
+//✓Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID 
+//✓ Debe tener un método deleteProduct, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo.
 
+const fs = require('fs');
 class ProductManager {
     constructor(filename) {
-        this.path = filename;
+        this.path = `./files/${filename}`
         this.format = 'utf-8';
     }
+
     getNextID = async () => {
         const list = await this.getProduct();
-        console.log('->',list.length);
         const nextID = (list.length > 0) ? list[list.length - 1].id + 1 : 1 //count-1 es el ultimo elemento del arreglo, tomamos su id y le sumamos 1, caso contrario si count = 0 id va a ser = a 1
         return nextID;
     }
+
     addProduct = async (title, description, price, thumbnail, code, stock) => {
             const newProduct = { id: await this.getNextID(), title, description, price, thumbnail, code, stock}
-            console.log(newProduct);
             const productsList = await this.getProduct();
+
             productsList.push(newProduct);
             await fs.promises.writeFile(this.path, JSON.stringify(productsList));
-        }
+    }
 
     getProduct = async () => {
         try {
             const data = await fs.promises.readFile(this.path, this.format);
-            const dataObj = JSON.parse(data)
-            
+            const dataObj = JSON.parse(data);
             return dataObj
         } catch (error) {
-            console.log('ERROR:',error);
+            console.log('No hay productos en la lista');
             return [];
         }
     }
-    
 
+    getProductByID = async (id) => {
+        const listOfProducts = await this.getProduct();
+        const product = listOfProducts.find(element => element.id === id);
+        
+        if (product) {
+            console.log('Se ha encontrado un producto con ID = ', id);
+            return product;
+        }
+        return ('Error: Not found')
+    }
+
+    updateProduct = async (id, updatedProps) => {
+        const listOfProducts = await this.getProduct();
+        const productIndex = listOfProducts.findIndex((element) => element.id === id);
+
+        if (productIndex !== -1) {
+            const updatedProduct = { ...listOfProducts[productIndex], ...updatedProps };
+            updatedProduct.id = id; // Evitar la actualización del ID
+            listOfProducts[productIndex] = updatedProduct;
+            await fs.promises.writeFile(this.path, JSON.stringify(listOfProducts));
+            console.log(`Producto con ID ${id} actualizado.`);
+        } else {
+            console.log(`Producto con ID ${id} no encontrado.`);
+        }
+    }
+
+    deleteProduct = async (id) => {
+        const products = await this.getProduct();
+        const updatedProducts = products.filter((product) => product.id !== id);
+    
+        if (updatedProducts.length === products.length) {
+            console.log(`Producto con ID ${id} no encontrado.`);
+        } else {
+            await fs.promises.writeFile(this.path, JSON.stringify(updatedProducts));
+            console.log(`Producto con ID ${id} eliminado.`);
+        }
+    }
 }
 async function run () {
     const productManager = new ProductManager('products.json');
+
     await productManager.addProduct('jabon', 'jabon de glicerina', 450, null, "JG1", 400);
     await productManager.addProduct('crema', 'crema para manos', 500, null, "CM1", 1300);
+    await productManager.addProduct('crema nivea', 'crema para piernas', 500, null, "CP2", 1250);
+    console.log(await productManager.getProduct());
+    console.log(await productManager.getProductByID(2));
+    console.log(await productManager.getProductByID(4));
+    await productManager.updateProduct(2, { title: 'Nuevo título', price: 1000, id:'123ABC' });
+    console.log(await productManager.getProduct());
+    await productManager.deleteProduct(1);
+    await productManager.addProduct('talco', 'talco para pies', 500, null, "TP1", 650);
     console.log(await productManager.getProduct());
 }
 run();
-
-// console.log(productManager.getProductByID(2));
-// console.log(productManager.getProductByID(3));
-
-
