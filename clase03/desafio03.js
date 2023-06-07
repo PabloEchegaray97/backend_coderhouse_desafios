00dfef1411ae3471a3013ba43c340e38cb87baa6
pae <pablo.echegaray97@gmail.com>
2023-06-07 07:35:38 -0300

    try-catch modif

--- clase03/desafio03.js
+++ clase03/desafio03.js
@@ -20,18 +20,27 @@ class ProductManager {
     }
 
     getNextID = async () => {
-        const list = await this.getProduct();
-        const nextID = (list.length > 0) ? list[list.length - 1].id + 1 : 1 //count-1 es el ultimo elemento del arreglo, tomamos su id y le sumamos 1, caso contrario si count = 0 id va a ser = a 1
-        return nextID;
-    }
+        try {
+            const list = await this.getProduct();
+            const nextID = (list.length > 0) ? list[list.length - 1].id + 1 : 1 //count-1 es el ultimo elemento del arreglo, tomamos su id y le sumamos 1, caso contrario si count = 0 id va a ser = a 1
+            return nextID;
+        } catch (error) {
+            console.log('Error al obtener el próximo ID:', error);
+            return null;
+        }
+    };
 
     addProduct = async (title, description, price, thumbnail, code, stock) => {
-            const newProduct = { id: await this.getNextID(), title, description, price, thumbnail, code, stock}
+        try {
+            const newProduct = { id: await this.getNextID(), title, description, price, thumbnail, code, stock }
             const productsList = await this.getProduct();
-
             productsList.push(newProduct);
             await fs.promises.writeFile(this.path, JSON.stringify(productsList));
-    }
+        } catch (error) {
+            console.log('Error al agregar el producto:', error);
+        }
+
+    };
 
     getProduct = async () => {
         try {
@@ -42,47 +51,58 @@ class ProductManager {
             console.log('No hay productos en la lista');
             return [];
         }
-    }
+    };
 
     getProductByID = async (id) => {
-        const listOfProducts = await this.getProduct();
-        const product = listOfProducts.find(element => element.id === id);
-        
-        if (product) {
-            console.log('Se ha encontrado un producto con ID = ', id);
-            return product;
+        try {
+            const listOfProducts = await this.getProduct();
+            const product = listOfProducts.find(element => element.id === id);
+            if (product) {
+                console.log('Se ha encontrado un producto con ID = ', id);
+                return product;
+            }
+            return 'Error: Not found'
+        } catch (error) {
+            return ('(!) Producto no encontrado, ', error)
         }
-        return ('Error: Not found')
-    }
+    };
 
     updateProduct = async (id, updatedProps) => {
-        const listOfProducts = await this.getProduct();
-        const productIndex = listOfProducts.findIndex((element) => element.id === id);
+        try {
+            const listOfProducts = await this.getProduct();
+            const productIndex = listOfProducts.findIndex((element) => element.id === id);
 
-        if (productIndex !== -1) {
-            const updatedProduct = { ...listOfProducts[productIndex], ...updatedProps };
-            updatedProduct.id = id; // Evitar la actualización del ID
-            listOfProducts[productIndex] = updatedProduct;
-            await fs.promises.writeFile(this.path, JSON.stringify(listOfProducts));
-            console.log(`Producto con ID ${id} actualizado.`);
-        } else {
-            console.log(`Producto con ID ${id} no encontrado.`);
+            if (productIndex !== -1) {
+                const updatedProduct = { ...listOfProducts[productIndex], ...updatedProps };
+                updatedProduct.id = id; // Evitar la actualización del ID
+                listOfProducts[productIndex] = updatedProduct;
+                await fs.promises.writeFile(this.path, JSON.stringify(listOfProducts));
+                console.log(`Producto con ID ${id} actualizado.`);
+            } else {
+                console.log(`Producto con ID ${id} no encontrado.`);
+            }
+        } catch (error) {
+            console.log('Error al actualizar el producto:', error);
         }
-    }
+    };
 
     deleteProduct = async (id) => {
-        const products = await this.getProduct();
-        const updatedProducts = products.filter((product) => product.id !== id);
-    
-        if (updatedProducts.length === products.length) {
-            console.log(`Producto con ID ${id} no encontrado.`);
-        } else {
-            await fs.promises.writeFile(this.path, JSON.stringify(updatedProducts));
-            console.log(`Producto con ID ${id} eliminado.`);
+        try {
+            const products = await this.getProduct();
+            const updatedProducts = products.filter((product) => product.id !== id);
+            if (updatedProducts.length === products.length) {
+                console.log(`Producto con ID ${id} no encontrado.`);
+            } else {
+                await fs.promises.writeFile(this.path, JSON.stringify(updatedProducts));
+                console.log(`Producto con ID ${id} eliminado.`);
+            }
+        } catch (error) {
+            console.log('Error al eliminar el producto:', error);
         }
-    }
-}
-async function run () {
+    };
+};
+
+async function run() {
     const productManager = new ProductManager('products.json');
 
     await productManager.addProduct('jabon', 'jabon de glicerina', 450, null, "JG1", 400);
@@ -91,10 +111,10 @@ async function run () {
     console.log(await productManager.getProduct());
     console.log(await productManager.getProductByID(2));
     console.log(await productManager.getProductByID(4));
-    await productManager.updateProduct(2, { title: 'Nuevo título', price: 1000, id:'123ABC' });
+    await productManager.updateProduct(2, { title: 'Nuevo título', price: 1000, id: '123ABC' });
     console.log(await productManager.getProduct());
     await productManager.deleteProduct(1);
     await productManager.addProduct('talco', 'talco para pies', 500, null, "TP1", 650);
     console.log(await productManager.getProduct());
-}
+};
 run();
